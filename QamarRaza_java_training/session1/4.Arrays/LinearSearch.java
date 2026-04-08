2f2af7551ea31097d041a566f2a58648fa950b6c
Qamar Raza <raza8bb805@gmail.com>
2026-04-08 17:41:31 +0530

    Create Linear Search program

--- QamarRaza_java_training/session1/4.Arrays/LinearSearch.java
+++ QamarRaza_java_training/session1/4.Arrays/LinearSearch.java
@@ -1,1 +1,25 @@
-impor
+import java.util.Scanner;
+class LinearSearch
+{
+    static void linearSearch(int a[],int target)
+    {
+        for(int i=0;i<a.length;i++)
+        {
+            if(target==a[i])
+            {
+                System.out.println("Element found at a["+i+"]");
+                return;
+            }
+        }
+        System.out.println("Element not found !!");
+    }
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = {10,46,23,6,37};
+        System.out.println("Enter an element to search: ");
+        int num = sc.nextInt();
+        linearSearch(arr,num);
+    }
+}
