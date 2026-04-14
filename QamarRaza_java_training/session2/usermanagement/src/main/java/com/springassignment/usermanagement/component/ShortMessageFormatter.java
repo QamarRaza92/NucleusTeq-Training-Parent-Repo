fe32cb4c6144580b69fe98b43f97a70b1ec65e1b
Qamar Raza <raza8bb805@gmail.com>
2026-04-14 16:14:37 +0530

    Create short message formatter component

---
+++ QamarRaza_java_training/session2/usermanagement/src/main/java/com/springassignment/usermanagement/component/ShortMessageFormatter.java
@@ -0,0 +1,12 @@
+package com.springassignment.usermanagement.component;
+
+import org.springframework.stereotype.Component;
+
+@Component 
+public class ShortMessageFormatter
+{
+    public String getMessage()
+    {
+        return "Short Message";
+    }
+}
