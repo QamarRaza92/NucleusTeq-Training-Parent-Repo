33ef8aeb2e0bc7c161ff862be18d1947309737e0
Qamar Raza <raza8bb805@gmail.com>
2026-04-16 00:10:26 +0530

    Add setter methods in todo class

--- QamarRaza_java_training/session4/taskmanager/src/main/java/com/springadvanced/taskmanager/entity/Todo.java
+++ QamarRaza_java_training/session4/taskmanager/src/main/java/com/springadvanced/taskmanager/entity/Todo.java
@@ -20,4 +20,27 @@ public class Todo
     private String status;
 
     private LocalDateTime createdAt;
+
+
+    //All getter setter methods
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+    public void setDescription(String status)
+    {
+        this.status = status;
+    }
+    public void setCreatedAt(LocalDateTime createdAt)
+    {
+        this.createdAt = createdAt;
+    }
 }
