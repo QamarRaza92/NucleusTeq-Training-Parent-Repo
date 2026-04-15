76566ef3df492745138dc6ef1028ffaf2fd706d4
Qamar Raza <raza8bb805@gmail.com>
2026-04-15 10:55:04 +0530

    Create User class with getter functions

---
+++ QamarRaza_java_training/session3/usermanagement/src/main/java/com/advancedspring/usermanagement/model/User.java
@@ -0,0 +1,16 @@
+package com.advancedspring.usermanagement.model;
+
+public class User 
+{
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private String email;
+
+    public int getId(){return id;}
+    public String getName(){return name;}
+    public int getAge(){return age;}
+    public String getGender(){return gender;}
+    public String getEmail(){return email;}
+}
