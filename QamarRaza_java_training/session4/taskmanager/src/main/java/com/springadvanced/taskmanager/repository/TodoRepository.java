d17c5d28f350b8eeb17bdc7de8b7162953a316eb
Qamar Raza <raza8bb805@gmail.com>
2026-04-16 00:17:06 +0530

    Add import Todo in repository

--- QamarRaza_java_training/session4/taskmanager/src/main/java/com/springadvanced/taskmanager/repository/TodoRepository.java
+++ QamarRaza_java_training/session4/taskmanager/src/main/java/com/springadvanced/taskmanager/repository/TodoRepository.java
@@ -1,7 +1,7 @@
 package com.springadvanced.taskmanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.springadvanced.taskmanager.entity.Todo;
 public interface TodoRepository extends JpaRepository<Todo, Long>
 {
 }
