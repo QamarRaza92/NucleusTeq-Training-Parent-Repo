f9c4ed5ef1fb0cf2b167287ed1d3e7c036851cab
Qamar Raza <raza8bb805@gmail.com>
2026-04-15 10:59:35 +0530

    Add default and parameterized constructor

--- QamarRaza_java_training/session3/usermanagement/src/main/java/com/advancedspring/usermanagement/model/User.java
+++ QamarRaza_java_training/session3/usermanagement/src/main/java/com/advancedspring/usermanagement/model/User.java
@@ -8,6 +8,20 @@ public class User
     private String gender;
     private String email;
 
+    //Create public constructor to set data
+    public User(int id,String name,int age,String gender,String email)
+    {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.email = email;
+    }
+
+    //Create No-Args constructor
+    public User(){}
+
+    //Create getter methods
     public int getId(){return id;}
     public String getName(){return name;}
     public int getAge(){return age;}
