b172d6b95695b18bbff50eba507261ac4feb45f5
Qamar Raza <raza8bb805@gmail.com>
2026-04-17 11:59:34 +0530

    Add DTO mapping in service layer

--- QamarRaza_java_training/session4/taskmanager/src/main/java/com/springadvanced/taskmanager/service/TodoService.java
+++ QamarRaza_java_training/session4/taskmanager/src/main/java/com/springadvanced/taskmanager/service/TodoService.java
@@ -5,47 +5,77 @@ import com.springadvanced.taskmanager.repository.TodoRepository;
 import java.util.List;
 import java.time.LocalDateTime;
 import com.springadvanced.taskmanager.entity.Todo;
+import com.springadvanced.taskmanager.dto.TodoRequestDTO;
+import com.springadvanced.taskmanager.dto.TodoResponseDTO;
 @Service 
 public class TodoService
 {
     private final TodoRepository todoRepository;
+    //Constructor Injection of Repository 
     public TodoService(TodoRepository todoRepository)
     {
         this.todoRepository = todoRepository;
     }
 
-    public Todo createTodo(Todo todo)
+
+    //Insert new task via DTO
+    public TodoResponseDTO createTodo(TodoRequestDTO dto)
     {
+        Todo todo = new Todo();
+        todo.setTitle(dto.getTitle());
+        todo.setDescription(dto.getDescription());
+        todo.setStatus("PENDING");
         todo.setCreatedAt(LocalDateTime.now());
-        return todoRepository.save(todo);
+        Todo saved = todoRepository.save(todo);
+        return convertToDTO(saved);
     }
-
-    public List<Todo> getAllTodos()
+    public TodoResponseDTO convertToDTO(Todo todo)
     {
-        return todoRepository.findAll();
+        TodoResponseDTO dto = new TodoResponseDTO();
+        dto.setId(todo.getId());
+        dto.setTitle(todo.getTitle());
+        dto.setStatus(todo.getStatus());
+        return dto;
     }
 
-    public Todo getTodoById(Long id)
+
+    //Get all tasks via DTO
+    public List<TodoResponseDTO> getAllTodos()
     {
-        return todoRepository.findById(id).orElse(null);
+        return todoRepository.findAll().stream().map(this::convertToDTO).toList();
+    }
+    
+
+
+    //Get task by id via DTO
+    public TodoResponseDTO getTodoById(Long id)
+    {
+        Todo todo = todoRepository.findById(id).orElse(null);
+        return (todo != null) ? convertToDTO(todo):null;
     }
 
+
+
+    //Delete a task via DTO
     public void deleteTodo(Long id)
     {
         todoRepository.deleteById(id);
     }
 
-    public Todo updateTodo(Long id, Todo updatedTodo)
+
+
+    //Update a tak via DTO
+    public TodoResponseDTO updateTodo(Long id, TodoRequestDTO  dto)
     {
         Todo existing = todoRepository.findById(id).orElse(null);
 
-        if(existing != null)
+        if(existing!=null)
         {
-            existing.setTitle(updatedTodo.getTitle());
-            existing.setDescription(updatedTodo.getDescription());
-            existing.setStatus(updatedTodo.getStatus());
-
-            return todoRepository.save(existing);
+            existing.setTitle(dto.getTitle());
+            existing.setDescription(dto.getDescription());
+            
+            Todo updated = todoRepository.save(existing);
+            return convertToDTO(updated);
         }
         return null;
     }
