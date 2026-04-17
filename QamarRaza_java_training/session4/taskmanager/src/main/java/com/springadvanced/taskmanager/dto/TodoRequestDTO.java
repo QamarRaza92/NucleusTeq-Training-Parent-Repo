7d037f34f50acdf56c56d12392a28646f2f51422
Qamar Raza <raza8bb805@gmail.com>
2026-04-17 12:20:32 +0530

    Add validation annotation(size and not null) in TodoRequestDTO

--- QamarRaza_java_training/session4/taskmanager/src/main/java/com/springadvanced/taskmanager/dto/TodoRequestDTO.java
+++ QamarRaza_java_training/session4/taskmanager/src/main/java/com/springadvanced/taskmanager/dto/TodoRequestDTO.java
@@ -1,7 +1,11 @@
 package com.springadvanced.taskmanager.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 public class TodoRequestDTO
 {
+    @NotNull(message = "Title cannot be null");
+    @Size(min=3,message="Title length must be 3 characters or more");
     private String title;
     private String description;
 
