b18304a297127b6cbc90fdf08f2b4b7c2f65ac8f
Qamar Raza <raza8bb805@gmail.com>
2026-04-18 16:59:12 +0530

    Add new global exception handlers i.e. RuntimeException,MethodArgumentNotValidException and Exception(which is the generic one)

--- QamarRaza_java_training/session5/taskmanager/src/main/java/com/springadvanced/taskmanager/exception/GlobalExceptionHandler.java
+++ QamarRaza_java_training/session5/taskmanager/src/main/java/com/springadvanced/taskmanager/exception/GlobalExceptionHandler.java
@@ -12,12 +12,52 @@ import java.time.LocalDateTime;
 @ControllerAdvice
 public class GlobalExceptionHanlder
 {
-    //1. Handling the customer exception
+    //1. Handling the custom exception
     @ExceptionHanlder(TodoNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleCustomException(TodoNotFoundException ex, WebRequest request)
     {
         ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(), 
                                                 ex.getMessage(), request.getDescription(false).replace("uri=",""));
         return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
+    }
+
+    //2. Handling exceptions coming from "@Valid"
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex,WebRequest request)
+    {
+        String errorMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        ErrorResponse error = new ErrorResponse
+                                              (
+                                                HttpStatus.BAD_REQUEST.value(),
+                                                errorMessage,
+                                                request.getDescription(false).replace("uri=","")
+                                              );
+        return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+    }
+
+    //3. Handling RuntimeException (catch all fallbacks)
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex, WebRequest request)
+    {
+        ErrorResponse error = new ErrorResponse
+                                               (
+                                                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                                                "INTERNAL SERVER ERROR "+ ex.getMessage(),
+                                                request.getDescription(false).replace("uri=","")
+                                               );
+        return new ResponseEntity<>(error,HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    //4. Handle Generic Exceptions 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex, WebRequest request)
+    {
+        ErrorResponse error = new ErrorResponse
+                                              (
+                                                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                                                ex.getMessage(),
+                                                request.getDescription(false).replace("uri=","")
+                                              );
+        return new ResponseEntity<>(error,HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
