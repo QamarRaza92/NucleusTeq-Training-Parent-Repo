25a7c10a9653c17d2d493844ffda162ff58f5638
Qamar Raza <raza8bb805@gmail.com>
2026-04-18 18:00:39 +0530

    Add Logger in controller layer

--- QamarRaza_java_training/session5/taskmanager/src/main/java/com/springadvanced/taskmanager/controller/TodoController.java
+++ QamarRaza_java_training/session5/taskmanager/src/main/java/com/springadvanced/taskmanager/controller/TodoController.java
@@ -9,10 +9,13 @@ import jakarta.validation.Valid;
 import com.springadvanced.taskmanager.exception.GlobalExceptionHandler;
 import com.springadvanced.taskmanager.exception.TodoNotFoundException;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @RestController
 @RequestMapping("/todos")
 public class TodoController
 {
+    private static final Logger log = LoggerFactory.getLogger(TodoController);
     private final TodoService todoService;
     public TodoController(TodoService todoService)
     {
@@ -22,36 +25,42 @@ public class TodoController
     @GetMapping //get all todos 
     public List<TodoResponseDTO> getAllTodos()
     {
+        log.info("GET request accepted to fetch all users...");
         return todoService.getAllTodos();
     }
 
     @GetMapping("/{id}")  //get todo by id 
     public TodoResponseDTO getTodoById(@PathVariable Long id)
     {
+        log.info("GET request accepted to fetch user: {}",id);
         return todoService.getTodoById(id);
     }
 
     @PostMapping //(post or insert) new todo 
     public TodoResponseDTO createTodo(@RequestBody @Valid TodoRequestDTO dto)
     {
+        log.info("POST request accepted to insert new user");
         return todoService.createTodo(dto);
     }
 
     @DeleteMapping("/{id}")  //delete todo by id 
     public void deleteTodo(@PathVariable Long id)
     {
+        log.info("DELETE request accepted to delete user: {}",id);
         todoService.deleteTodo(id);
     }
 
     @PutMapping("{id}")  //(put or update) todo by id
     public TodoResponseDTO updateTodo(@PathVariable Long id, @RequestBody @Valid TodoRequestDTO dto)
     {
+        log.info("PUT request accepted to update user: {}",id);
         return todoService.updateTodo(id,dto);
     }
 
     @PatchMapping("/toggleStatus/{id}")  //toggle status of todo 
     public TodoResponseDTO toggleStatus(@PathVariable Long id)
     {
+        log.info("PATCH request accepted to update user: {}",id);
         return todoService.toggleStatus(id);
     }
 }
