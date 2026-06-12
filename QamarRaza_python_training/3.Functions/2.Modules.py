d40f6405a1e518a31fdbef820141d165ac7eb316
Qamar Raza <raza8bb805@gmail.com>
2026-06-12 09:13:04 +0530

    Use import to utilize inbuilt modules

---
+++ QamarRaza_python_training/3.Functions/2.Modules.py
@@ -0,0 +1,14 @@
+# 22. Use math module to find square root, power, and factorial.
+import math 
+sqrt = math.sqrt(16)
+power = math.pow(3,2)
+factorial  = math.factorial(5)
+print(f"Square Root: {sqrt}\nPower: {power}\nFactorial: {factorial}")
+print("-"*30)
+
+# 23. Generate random numbers using random module.
+import random
+print("Random number between 1 and 10: ",random.randint(1,10))
+print("-"*30)
+
+# 24. Create your own module and import it.
