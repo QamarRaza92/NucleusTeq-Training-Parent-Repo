66b71e44840603cec9b9e62b429bd8868e44057e
Qamar Raza <raza8bb805@gmail.com>
2026-06-12 08:52:17 +0530

    Solve introductory questions

---
+++ QamarRaza_python_training/1.Introduction/main.py
@@ -0,0 +1,13 @@
+# 1. Write a program to print 'Welcome to Python Training'.
+print("Welcome to Python Training")
+print("-"*30)
+
+# 2. Write a program to check your Python version.
+import sys
+print(sys.version)
+print("-"*30)
+
+# 3. Take user input (name and age) and print a formatted message.
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"Name: {name}\nAge: {age}")
