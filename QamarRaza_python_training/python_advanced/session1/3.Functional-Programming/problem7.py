8088ebc803a02ee51a32567229eb37c437022482
Qamar Raza <raza8bb805@gmail.com>
2026-06-18 11:43:57 +0530

    Functional-Programming: Simple loop into functional style program

---
+++ QamarRaza_python_training/python_advanced/session1/3.Functional-Programming/problem7.py
@@ -0,0 +1,5 @@
+# Q7. Convert a simple loop-based program into a functional style using map or filter.
+print("Q7. Convert a simple loop-based program into a functional style using map or filter.")
+nums = [1, 2, 3, 4, 5]
+squares = list(map(lambda x: x * x, nums))
+print(squares)
