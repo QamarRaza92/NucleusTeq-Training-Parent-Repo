8d3986eeade322a4938cdd0e50632363edd39d16
Qamar Raza <raza8bb805@gmail.com>
2026-06-18 11:34:31 +0530

    Use built-in generator (enumerate)

---
+++ QamarRaza_python_training/python_advanced/session1/2.Iterators-And-Generators/problem8.py
@@ -0,0 +1,7 @@
+# 8. Show an example of a built-in generator (like range) and iterate over it.
+print("Q8. Show an example of a built-in generator (like range) and iterate over it.")
+built_in_gen = enumerate(["Qamar","Kallis","Kohli","Dhoni","Lara","Sobers"])
+print(next(built_in_gen))
+print(next(built_in_gen))
+print(next(built_in_gen))
+print(next(built_in_gen))
