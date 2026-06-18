41ccd1355bfc90d17c0c3ead307ff7e044e65f0d
Qamar Raza <raza8bb805@gmail.com>
2026-06-18 11:31:28 +0530

    Use iterator for list

---
+++ QamarRaza_python_training/python_advanced/session1/2.Iterators-And-Generators/problem1.py
@@ -0,0 +1,6 @@
+# 1. Create an iterator for a list and print elements using next().
+print("Q1. Create an iterator for a list and print elements using next().")
+my_iter = iter([10,20,30])
+print(next(my_iter))
+print(next(my_iter))
+print(next(my_iter))
