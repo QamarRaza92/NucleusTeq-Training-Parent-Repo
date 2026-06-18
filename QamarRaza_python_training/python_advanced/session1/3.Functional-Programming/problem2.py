8e078d69ddd49288c4311be776d1a322c77f75cb
Qamar Raza <raza8bb805@gmail.com>
2026-06-18 11:40:08 +0530

    Functional-Programming: Use 'map' function to cal square

---
+++ QamarRaza_python_training/python_advanced/session1/3.Functional-Programming/problem2.py
@@ -0,0 +1,7 @@
+# Q2. Use map() to convert a list of numbers into their squares.
+print("Q2. Use map() to convert a list of numbers into their squares.")
+squares = map(lambda x: x * x, [1, 2, 3, 4])
+print(next(squares))
+print(next(squares))
+print(next(squares))
+print(next(squares))
