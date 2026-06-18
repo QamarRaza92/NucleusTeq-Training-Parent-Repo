4d1b8fedfbffa53bf84707cb3e97b3a0f3dd9c1a
Qamar Raza <raza8bb805@gmail.com>
2026-06-18 12:26:44 +0530

    Packaging: Diff between module and package with example

---
+++ QamarRaza_python_training/python_advanced/session1/6.Packaging/problem2.py
@@ -0,0 +1,7 @@
+# Q2. Explain the difference between a module and a package with an example.from package1 import mod1
+print("Q2. Explain the difference between a module and a package with an example.")
+from package1 import mod1
+
+print("using package: ",mod1.greet("Qamar"))
+import module1
+print("Using Module: ",module1.to_upper("qamar"))
