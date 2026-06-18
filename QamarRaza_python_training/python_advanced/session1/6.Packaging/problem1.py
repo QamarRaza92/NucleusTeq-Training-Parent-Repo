7872bd0105aefa262b6f057d0f195a68baa3e704
Qamar Raza <raza8bb805@gmail.com>
2026-06-18 12:19:58 +0530

    Packaging: Import a module in another file

---
+++ QamarRaza_python_training/python_advanced/session1/6.Packaging/problem1.py
@@ -0,0 +1,6 @@
+# Q1. Create a module with two utility functions and import it into another Python file.
+print("Q1. Create a module with two utility functions and import it into another Python file.")
+import module1
+print(module1.to_lower("Qamar"))
+print(module1.to_upper("qamar"))
+print("-"*50)
