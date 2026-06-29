fdd17bcdc73e6e9d53909f4bc97495caf64c7dd6
Qamar Raza <raza8bb805@gmail.com>
2026-06-29 19:29:43 +0530

    Add routes for login, register and dashboard pages

--- Capstone_CircleUp/backend/app/main.py
+++ Capstone_CircleUp/backend/app/main.py
@@ -5,6 +5,8 @@ from app.routers import users
 from app.routers import activities
 from app.routers import participations
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 Base.metadata.create_all(bind=engine)
 
@@ -17,11 +19,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.mount("/static", StaticFiles(directory="../frontend"), name="static")
+
 app.include_router(auth.router)
 app.include_router(users.router)
-# app.include_router(activities.router)
-# app.include_router(participations.router)
+app.include_router(activities.router)
+app.include_router(participations.router)
 
 @app.get("/")
 def root():
-    return {"Message":"Testing DB connection."}
+    return FileResponse("../frontend/html/login.html")
+
+@app.get("/register")
+def register():
+    return FileResponse("../frontend/html/register.html")
+
+@app.get("/dashboard")
+def dashboard():
+    return FileResponse("../frontend/html/dashboard.html")
