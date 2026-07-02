1b90cbd2da9aeba743685797e0f9508d9c2c670b
Qamar Raza <raza8bb805@gmail.com>
2026-07-02 11:29:27 +0530

    Add some validations in schema and remove extra comments

--- Capstone_CircleUp/backend/app/schemas/register_dto.py
+++ Capstone_CircleUp/backend/app/schemas/register_dto.py
@@ -3,12 +3,12 @@ from typing import Optional
 import re 
 
 class RegisterRequest(BaseModel):
-    name: str = Field(...,title="name", description="Name to login",examples=["Qamar Raza","Chandler Bing","Michael Scott"])
+    name: str = Field(...,title="name", min_length=3,max_length=50,description="Name to login",examples=["Qamar Raza","Chandler Bing","Michael Scott"])
     email: EmailStr
     password: str = Field(...,title="password",min_length=8,max_length=15,
                     description="Password must be 8-15 characters long, contain 1 number, and 1 special character.",
                     examples=["wxyz@123"])
-    phone_number: str = Field(...,title="phone",description="user's phone number")
+    phone_number: str = Field(...,title="phone",description="user's phone number",min_length=10,max_length=10)
     city: str = Field(...,title="city",description="user's city",examples=["Indore","New Delhi","Mumbai"])
     bio: Optional[str] = Field(default=None,title="bio",description="user bio")
 
@@ -29,6 +29,18 @@ class RegisterRequest(BaseModel):
         if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
             raise ValueError("Password must contain at least 1 special character.")
         return password
+    
+    @field_validator("phone_number")
+    @classmethod
+    def validate_indian_phone_number(cls, phone_number: str) -> str:
+        phone_number = phone_number.strip()
+        if not phone_number.isdigit():
+            raise ValueError("Phone number must contain only digits.")
+        if len(phone_number) != 10:
+            raise ValueError("Phone number must be exactly 10 digits.")
+        if not phone_number.startswith(("6", "7", "8", "9")):
+            raise ValueError("Invalid Indian mobile number.")
+        return phone_number
     
 
 class RegisterResponse(BaseModel):
